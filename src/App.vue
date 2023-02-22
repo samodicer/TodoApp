256f1b54f8d4323dbb81b24cf600546b5b7d367f
Samuel Dicer <54637954+samodicer@users.noreply.github.com>
2023-02-22 19:08:42 +0100

    Layout
    added app layout

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,51 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+  <v-app class="bg">
+    <v-app-bar flat app>
+      <v-container class="fill-height d-flex align-center">
+        <v-avatar class="me-10 ms-4" color="grey" size="32"></v-avatar>
+        <v-spacer></v-spacer>
+        Meno
+      </v-container>
     </v-app-bar>
-
-    <v-main>
-      <router-view/>
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item v-for="n in 5" :key="n" link>
+                  <v-list-item-title> List Item {{ n }} </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+          <v-col>
+            <v-sheet min-height="80vh" rounded="lg">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
+    <v-footer
+      min-height="10vh"
+      class="bg-indigo-lighten-1 text-center d-flex flex-column"
+    >
+      <p>Footer</p>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
   data: () => ({
-    //
+    links: ["Dashboard", "Messages", "Profile", "Updates"],
   }),
 };
 </script>
+
+<style scoped>
+.bg {
+  background-color: grey;
+}
+</style>
