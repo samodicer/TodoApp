612da76b9a1afeccea49e1fce6a1881852cbfea1
Samuel Dicer <54637954+samodicer@users.noreply.github.com>
2023-02-23 11:45:55 +0100

    Login page
    added login inputs and validation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,65 @@
 <template>
-  <p>Login</p>
+  <v-form v-model="isFormValid">
+    <v-text-field
+      v-model="user.email"
+      tabindex="1"
+      label="Email"
+      placeholder="Email"
+      :rules="[rules.required]"
+      outlined
+      color="#26A69A"
+    ></v-text-field>
+    <v-text-field
+      v-model="user.password"
+      tabindex="1"
+      :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+      :rules="[rules.required]"
+      :type="showPassword ? 'text' : 'password'"
+      label="Password"
+      hint="At least 8 characters"
+      outlined
+      @click:append="showPassword = !showPassword"
+      color="#26A69A"
+    ></v-text-field>
+    <v-btn
+      class="white--text"
+      tabindex="2"
+      color="#26A69A"
+      :disabled="!isFormValid"
+      @click="login()"
+    >
+      Login
+    </v-btn>
+  </v-form>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "LoginView",
+  data() {
+    return {
+      user: {
+        email: "",
+        password: "",
+      },
+      showPassword: false,
+      isFormValid: false,
+      rules: {
+        required: (value) => !!value || "This field is required",
+      },
+    };
+  },
+  methods: {
+    ...mapActions({
+      userLogin: "user/userLogin",
+    }),
+    login() {
+      this.userLogin(this.user).then(() => {
+        this.$router.push({ name: "todos" });
+      });
+    },
+  },
 };
 </script>
