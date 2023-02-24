ef386bbe98e01aa46afdc2176cfbb1fee6f5c7ef
Samuel Dicer <54637954+samodicer@users.noreply.github.com>
2023-02-24 20:16:11 +0100

    Translation added
    Added i18n for transalation

--- src/views/TranslateView.vue
+++ src/views/TranslateView.vue
@@ -1,9 +1,33 @@
 <template>
-  <p>Translate</p>
+  <v-card class="pa-6" flat>
+    <v-switch v-model="toggled" label="CZ" inset></v-switch>
+    <p>{{ $t("message") }}</p>
+  </v-card>
 </template>
 
 <script>
 export default {
   name: "TranslateView",
+  data() {
+    return {
+      toggled: false,
+    };
+  },
+  watch: {
+    toggled(newVal) {
+      if (newVal) {
+        this.$i18n.locale = "cz";
+      } else {
+        this.$i18n.locale = "en";
+      }
+    },
+  },
+  mounted() {
+    if (this.$i18n.locale == "en") {
+      this.toggled = false;
+    } else {
+      this.toggled = true;
+    }
+  },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ module.exports = {
   devServer: {
     proxy: "https://prodapi.juno.one",
   },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+      enableBridge: false
+    }
+  }
 };
