feb16064bd254ccc3cda37cddd34fc27114eebf1
Samuel Dicer <54637954+samodicer@users.noreply.github.com>
2023-02-25 15:18:56 +0100

    Logout
    Logout added

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,38 +1,44 @@
 <template>
   <v-form class="d-flex justify-center pa-6" v-model="isFormValid">
     <v-card flat width="600">
-      <v-card-title class="justify-center">LOGIN</v-card-title>
-      <v-text-field
-        v-model="user.email"
-        tabindex="1"
-        label="Email"
-        placeholder="Email"
-        :rules="[rules.required, rules.max]"
-        outlined
-        color="primary"
-      ></v-text-field>
-      <v-text-field
-        v-model="user.password"
-        tabindex="1"
-        label="Password"
-        placeholder="Password"
-        :rules="[rules.required, rules.max]"
-        :type="showPassword ? 'text' : 'password'"
-        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-        @click:append="showPassword = !showPassword"
-        outlined
-        color="primary"
-      ></v-text-field>
-      <div class="text-center">
-        <v-btn
-          class="justify-center"
-          tabindex="2"
+      <div v-if="isAuth" class="text-center">
+        <v-card-title class="justify-center">You are logged in.</v-card-title>
+        <v-icon color="primary" size="52" @click="logout()">mdi-logout</v-icon>
+      </div>
+      <div v-else>
+        <v-card-title class="justify-center">LOGIN</v-card-title>
+        <v-text-field
+          v-model="user.email"
+          tabindex="1"
+          label="Email"
+          placeholder="Email"
+          :rules="[rules.required, rules.max]"
+          outlined
           color="primary"
-          :disabled="!isFormValid"
-          @click="login()"
-        >
-          Login
-        </v-btn>
+        ></v-text-field>
+        <v-text-field
+          v-model="user.password"
+          tabindex="1"
+          label="Password"
+          placeholder="Password"
+          :rules="[rules.required, rules.max]"
+          :type="showPassword ? 'text' : 'password'"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+          outlined
+          color="primary"
+        ></v-text-field>
+        <div class="text-center">
+          <v-btn
+            class="justify-center"
+            tabindex="2"
+            color="primary"
+            :disabled="!isFormValid"
+            @click="login()"
+          >
+            Login
+          </v-btn>
+        </div>
       </div>
     </v-card>
   </v-form>
@@ -46,6 +52,7 @@ export default {
   name: "LoginView",
   data() {
     return {
+      isAuth: false,
       user: {
         email: "",
         password: "",
@@ -76,6 +83,7 @@ export default {
           .then((response) => {
             console.log(response);
             localStorage.setItem("authToken", response.headers.authorization);
+            this.isAuth = true;
             resolve();
           })
           .catch((err) => {
@@ -85,6 +93,18 @@ export default {
       });
       this.$router.push({ name: "todos" });
     },
+    checkToken() {
+      return localStorage.getItem("authToken") != null
+        ? (this.isAuth = true)
+        : (this.isAuth = false);
+    },
+    logout() {
+      localStorage.removeItem("authToken");
+      this.isAuth = false;
+    },
+  },
+  mounted() {
+    this.checkToken();
   },
 };
 </script>
